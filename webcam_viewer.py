54ce731e3fc4449f20175a42ca3cec230790baf0
Christian Rasmussen <31134924+Rasmussen-Christian@users.noreply.github.com>
2020-02-03 14:25:13 -0600

    added webcam_viewer.py
    Is used to show the video feed of the camera to help postion the item in front of the camera.
    Which `webcam_recorder` takes pictures of

---
+++ webcam_viewer.py
@@ -0,0 +1,28 @@
+from cv2 import cv2
+import time
+
+def main():
+    '''
+        shows the view of the webcam in a popup screen
+        press `esc´ to exit program
+    '''
+    cam = cv2.VideoCapture(0)
+    
+    while True:
+        # reads the camera
+        _, img = cam.read()
+
+        # shows it to the viewer in a pop up window
+        cv2.imshow('my webcam', img)
+
+        # breaks the loop when the user presses esc
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+
+    cv2.destroyAllWindows()
+
+# Call to main function to run the program
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    print("execution time:", time.time() - start)
