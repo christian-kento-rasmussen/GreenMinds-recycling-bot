8ae56bfe1882d0adf6dc82d0b838767bbb8c1eb9
Christian <Christian.kento@gmail.com>
2020-03-01 15:36:28 -0600

    Added graphics to main window and made load functions in .ipynd
    Updated the view to include graphics from open game art,
    made a load and safe function in the Jupiter notebook which do not require internet access

--- tkinter_app.py
+++ tkinter_app.py
@@ -25,7 +25,7 @@ class TKinterApp:
 
         # instantiates the NN
         self.green_minds_model = GreenMindsModel(os.path.join(sys.path[0], 'assets/model/densenet_checkpoint.pth'))
-        
+       
         # loads the json file that contains data for all of the objects
         with open(os.path.join(sys.path[0], 'items.json'), 'r') as json_file:
             self.items = json.load(json_file)
@@ -39,31 +39,37 @@ class TKinterApp:
         '''
             Creates the layout for the app using tkinter
         '''
+        # sets up the view
         self.root = tk.Tk()
         self.root.title('GreenMinds - Recycling app')
         self.root.geometry('1422x800')
+        self.root.configure(background='#1bcc00')
+        self.root.resizable(False, False)
 
-        # creates the title text
-        self.frame_title = tk.Frame(self.root, bg="#80c1ff")
-        self.frame_title.place(relx=0.5, rely=.05, relwidth=0.75, relheight=0.1, anchor="n")
-        self.title = tk.Label(self.frame_title, text="The recycling game", bg="#80c1ff")
-        self.title.place(relwidth=1, relheight=1)
+        # creates the bg
+        self.bg_image = ImageTk.PhotoImage(Image.open('assets/gui/root_bg.png'))
+        self.bg = tk.Label(self.root, image=self.bg_image)
+        self.bg.place(relwidth=1, relheight=1)
 
-        # creates the view of the camera
-        self.frame_video = tk.Frame(self.root, bg="#80c1ff")
-        self.frame_video.place(relx=0.5, rely=.25, relwidth=0.75, relheight=0.5, anchor="n")
+        # creates the title bar
+        self.title_image = ImageTk.PhotoImage(Image.open('assets/gui/GreenMinds_title.png'))
+        self.title_bg = tk.Label(self.root, image=self.title_image)
+        self.title_bg.place(width=750, height=75, anchor="n", relx=.5, rely=.035)
 
-        # initiating the panel
-        self.panel = tk.Label(self.root)
-        self.panel.place(relx=.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+        # creates the view for the camera
+        self.camera_bg_image = ImageTk.PhotoImage(Image.open('assets/gui/camera_bg.png'))
+        self.camera_bg = tk.Label(self.root, image=self.camera_bg_image)
+        self.camera_bg.place(width=800, height=450, relx=.5, anchor="n", y=150)
+        self.panel_video = tk.Label(self.camera_bg)
+        self.panel_video.place(relx=.5, rely=0.075, relwidth=.9, relheight=.8, anchor='n')
 
         # creates the view for the buttons about recyclebel
-        self.frame_buttons = tk.Frame(self.root, bg="#80c1ff", bd=5)
-        self.frame_buttons.place(relx=0.5, rely=0.88, relwidth=0.5, relheight=0.1, anchor="n")
-        self.not_recycle_button = tk.Button(self.frame_buttons, text="This is not recyclable!", command=lambda: (self.button_clicked(False)))
-        self.not_recycle_button.place(relwidth=.45, relheight=1)
-        self.recycle_button = tk.Button(self.frame_buttons, text="This is recyclable!", command=lambda: self.button_clicked(True))
-        self.recycle_button.place(relx=.55, relwidth=.45, relheight=1)
+        self.not_recycle_button_image = ImageTk.PhotoImage(Image.open('assets/gui/btn_is_recyclable.png'))
+        self.not_recycle_button = tk.Button(self.root, image=self.not_recycle_button_image, command=lambda: (self.button_clicked(False)))
+        self.not_recycle_button.place(width=380, height=98, relx=.3, rely=.96, anchor="s")
+        self.recycle_button_image = ImageTk.PhotoImage(Image.open('assets/gui/btn_not_recyclable.png'))
+        self.recycle_button = tk.Button(self.root, image=self.recycle_button_image, command=lambda: self.button_clicked(True))
+        self.recycle_button.place(width=380, height=98, relx=.7, rely=.96, anchor="s")
 
     def camera1(self):
         '''
@@ -75,9 +81,9 @@ class TKinterApp:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = Image.fromarray(frame)
         frame = ImageTk.PhotoImage(frame)
-        self.panel.configure(image=frame)
-        self.panel.image = frame
-        self.panel.after(1, self.camera1)
+        self.panel_video.configure(image=frame)
+        self.panel_video.image = frame
+        self.panel_video.after(1, self.camera1)
 
     def button_clicked(self, recyclebel):
         '''
@@ -95,7 +101,7 @@ class TKinterApp:
 
         print(prediction_procent)
         if prediction_procent < .95:
-            self.title.config(text='Please try to align in middle of camera')
+            self.show_popup_for(2000, 'Please try to align in middle of camera', self.root)
             return
 
         if recyclebel:  # the user things that it is recyclebel
