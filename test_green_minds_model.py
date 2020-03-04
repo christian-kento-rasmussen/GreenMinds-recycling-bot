471f41c2ea50782a79871752eb47fdb79a9bc1b9
Christian <Christian.kento@gmail.com>
2020-03-03 18:57:45 -0600

    Moved test_green_minds_model.py

---
+++ test_green_minds_model.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import unittest
+from PIL import Image
+from green_minds_model import GreenMindsModel
+
+
+class TestGreenMindsModel(unittest.TestCase):
+
+    def test_model_load(self):
+        """
+            Loads a PyTorch model from a path
+        """
+        green_minds_model = GreenMindsModel(os.path.join(sys.path[0], "assets/model/densenet_checkpoint.pth"))
+        self.assertTrue(True)
+
+    def test_model_inference(self):
+        """
+            Tests that the loaded PyTorch model runs inference correctly
+        """
+        green_minds_model = GreenMindsModel(os.path.join(sys.path[0], "assets/model/densenet_checkpoint.pth"))
+        self.assertEqual(green_minds_model.predict(Image.open(os.path.join(sys.path[0], "pytest/waterbottle.jpg")), topk=1)[1][0], "WaterBottle")
+
+
+if __name__ == '__main__':
+    unittest.main()
